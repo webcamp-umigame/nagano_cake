efa1f9d36af50c304928ed112487c71b3e714730
kmtszky <kmtszk711@gmail.com>
2021-04-18 13:17:41 +0000

    [Fix] layout, routing修正

--- app/controllers/customer/deliveries_controller.rb
+++ app/controllers/customer/deliveries_controller.rb
@@ -1,5 +1,6 @@
 class Customer::DeliveriesController < ApplicationController
-  before_action :set_delivery, only: [:index, :edit, :update, :destroy]
+
+  before_action :set_shipping_info, only: [:edit, :update, :destroy]
 
   # 配送先登録/一覧画面
   def index
@@ -11,7 +12,7 @@ class Customer::DeliveriesController < ApplicationController
   def create
     @delivery = current_customer.deliveries.new(delivery_params)
     if @delivery.save
-      redirect_to :index, notice: '配送先を更新しました！'
+      redirect_to deliveries_path, flash: {success: "配送先を登録しました！"}
     else
       @deliveries = Delivery.all
       render :index
@@ -25,7 +26,7 @@ class Customer::DeliveriesController < ApplicationController
   # 配送先編集画面 => 配送先更新
   def update
     if @delivery.update(delivery_params)
-      redirect_to :index, notice: '配送先を更新しました！'
+      redirect_to deliveries_path, flash: {success: "配送先を更新しました！"}
     else
       render :edit
     end
@@ -34,14 +35,13 @@ class Customer::DeliveriesController < ApplicationController
   # 配送先登録/一覧画面 => 配送先削除
   def destroy
     @delivery.destroy
-    redirect_to :index, notice: '配送先を削除しました！'
+    redirect_to deliveries_path, flash: {warning: "配送先を削除しました！"}
   end
 
   private
 
-  def set_delivery
-    binding.pry
-    @delivery = current_customer.deliveries.find(params[:id])
+  def set_shipping_info
+    @delivery = Delivery.find(params[:id])
   end
 
   def delivery_params
