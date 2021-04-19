ebbbf68799ec9919698aa03934642c1760c8658b
kmtszky <kmtszk711@gmail.com>
2021-04-19 05:46:49 +0000

    [update] order_product.rb => enumにて製作ステータス定義

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,12 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  # 製作ステータス（default: 0）
+  enum production_status: {
+    "着手不可": 0,
+    "製作待ち": 1,
+    "製作中": 2,
+    "制作完了": 3,
+  }
 end
