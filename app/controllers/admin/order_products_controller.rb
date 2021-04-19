6228dfb871bae6325456598742eb44c5593998e6
kmtszky <kmtszk711@gmail.com>
2021-04-19 06:24:10 +0000

    [update] admin：注文履歴詳細関連のcontroller, show更新

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,2 +1,6 @@
 class Admin::OrderProductsController < ApplicationController
+
+  def update
+    
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,23 @@
 class Admin::OrdersController < ApplicationController
+  before_action :set_order
+
+  def show
+    @customer_name = Customer.last_name + Customer.first_name
+    # 注文IDに紐づく、OrderProductのデータを全件取得
+    @order_products = OrderProduct.Where(order_id: params[:id])
+  end
+
+  def update
+    @order_status = @order.order_status
+    # @production_status = OrderProduct.find_by()
+    if @order_status.save
+
+    end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
 end
