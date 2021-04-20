baabb27dafb7eb57e88ae54178aec149cf14de2b
knmsy1125 <knmsy1125@gmail.com>
2021-04-20 05:52:29 +0000

    order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,18 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products
+  has_many :order_details, dependent: :destroy
 
+	validates :customer, :address,:name, :postage,
+			  		:total_price, :payment_method,
+			  		presence: true
+	validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
+	validates :postage, :total_price, numericality: { only_integer: true }
+
+	enum payment_method: {
+	  "クレジットカード": 0,
+	  "銀行振込": 1,
+	}
   # 注文ステータス（default: 0）
   enum order_status: {
     "入金待ち": 0,
