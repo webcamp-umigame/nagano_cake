c862d213ed6055d0135ac3c78f7380a4c97fe086
komayuka <komayuka115@gmail.com>
2021-04-23 11:22:02 +0000

    [test]修正

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -22,7 +22,7 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new(product_params)
     @product.save
       #　調べる
-     redirect_to admin_products_path
+     redirect_to admin_product_path(@product.id)
   end
 
   # 商品編集
