3f78eeac0da394bdbbda3562454e818483aaa8e5
komayuka <komayuka115@gmail.com>
2021-04-25 03:12:47 +0000

    [top]スライド表示

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('.slider').slick({
+    prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
+    nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
+    autoplay:true
+ });
+});
